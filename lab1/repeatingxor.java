089a79486bfa946c86969d0a7a660173bf803a12
CyberSeagull <62190177+CyberSeagull@users.noreply.github.com>
2021-12-21 22:44:03 +0200

    Update repeatingxor.java

--- lab1/repeatingxor.java
+++ lab1/repeatingxor.java
@@ -1,20 +1,14 @@
 
 public class repeatingxor {
-	public static byte[] decrypt (String text, String key)
+	public static String decrypt (String text, String key)
     {
-        byte [] res = new byte [text.length()];
+        String result = "";
         byte [] btext = text.getBytes();
         byte [] skey = key.getBytes();
         for (int i = 0; i<text.length();i++){
-            res[i] = (byte) (btext[i]^skey[i % key.length()]);
+            result+= String.format("%X", 0xF &((byte) (btext[i]^skey[i % key.length()])));
         }
-        return (res);
+        return result;
     }
 	
-
-
-public static void main(String[] args) {
-	System.out.println(decrypt("1207554F07181D45041C1A1E43534212131D4E134445541F0D4900014F140A444D120D48014E104F011255190C011C00", "7468276f7070726573736f7227732077726f6e67"));
-
-}
 }
