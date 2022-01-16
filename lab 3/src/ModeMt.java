d1c6838d69997d35e666e606b96ec733b9b8af9e
Viktoriia Zhezhera <deletrea.to@gmail.com>
2022-01-16 02:07:22 +0200

    seed added

--- lab 3/src/ModeMt.java
+++ lab 3/src/ModeMt.java
@@ -13,21 +13,31 @@ public class ModeMt {
 		 cr= new clientR();
 	}
 	
-    public void calculatioMt()  { 
+    public void calculationMt()  { 
 		try {
     	newacc=cr.getplayerid(pathuri, g);
-    	for(int i=0; i<2; i++) {
-    		String sadd=pathuri+String.format("/playMt?id=%d&bet=%d&number=%d", newacc.id, 1, i);
-		//	System.out.println( sadd );
+    		String sadd=pathuri+String.format("/playMt?id=%d&bet=%d&number=%d", newacc.id, 1, 5);
+			System.out.println( sadd );
     		String str = cr.get(sadd);
-//    		System.out.println( str );
-				
+    		System.out.println( str );
     		StplayMT newplay = g.fromJson(str, StplayMT.class);
+            long seed = System.currentTimeMillis()/1000 - 7;
+
+            RandomMt mt= new RandomMt();
+            long realNumber = newplay.realNumber;
+
+            mt.seed_mt((int) seed);
+            long  mtNumber=Integer.toUnsignedLong(mt.getnumber());
+            while ( mtNumber != realNumber ) {
+            	mt.seed_mt((int)++seed);
+            	 mtNumber=Integer.toUnsignedLong(mt.getnumber());
+                System.out.println(mtNumber+"   "+realNumber+" seed: " + seed);
+            }
+            
 	        System.out.println( g.toJson(newplay) );
 	        
 	        
-	        
-		} 
+		 
 		}catch (Exception e) { e.printStackTrace(); }
     	
     }
