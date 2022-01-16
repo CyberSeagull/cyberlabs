85b07684ee1268123b5c9927c0f742e8fcc59236
Viktoriia Zhezhera <deletrea.to@gmail.com>
2022-01-16 19:41:14 +0200

    added the game & fixed the tabs

--- lab 3/src/ModeMt.java
+++ lab 3/src/ModeMt.java
@@ -1,21 +1,20 @@
-
-
 import com.google.gson.Gson;
 
+
 public class ModeMt {
-	static final int bet = 100; 
+	static final int bet = 100;
 	Gson g = new Gson();
 	String pathuri;
 	clientR cr;
 	Craccount newacc;
-	public ModeMt(String pathuri){
-		this.pathuri=pathuri;
+	public ModeMt(String pathuri) {
+		this.pathuri = pathuri;
 		 cr= new clientR();
 	}
-	
-    public void calculationMt()  { 
+
+  	public void calculationMt()  {
 		try {
-    	newacc=cr.getplayerid(pathuri, g);
+			newacc=cr.getplayerid(pathuri, g);
     		String sadd=pathuri+String.format("/playMt?id=%d&bet=%d&number=%d", newacc.id, 1, 5);
 			System.out.println( sadd );
     		String str = cr.get(sadd);
@@ -24,21 +23,41 @@ public class ModeMt {
             long seed = System.currentTimeMillis()/1000 - 7;
 
             RandomMt mt= new RandomMt();
-            long realNumber = newplay.realNumber;
 
+            long realNumber = newplay.realNumber;
             mt.seed_mt((int) seed);
-            long  mtNumber=Integer.toUnsignedLong(mt.getnumber());
+            long  mtNumber = Integer.toUnsignedLong(mt.getnumber());
+            int i = 0;
+
             while ( mtNumber != realNumber ) {
             	mt.seed_mt((int)++seed);
-            	 mtNumber=Integer.toUnsignedLong(mt.getnumber());
+            	mtNumber=Integer.toUnsignedLong(mt.getnumber());
+                i++;
                 System.out.println(mtNumber+"   "+realNumber+" seed: " + seed);
+                if (i>100) {
+                	seed =- 1;
+                	break;
+                }
             }
-            
-	        System.out.println( g.toJson(newplay) );
-	        
-	        
-		 
-		}catch (Exception e) { e.printStackTrace(); }
-    	
+
+//  if seed<0 return seed;
+            do {
+        		mtNumber=Integer.toUnsignedLong(mt.getnumber());
+            	sadd=pathuri+String.format("/playMt?id=%d&bet=%d&number=%d", newacc.id, bet, mtNumber);
+    			System.out.println( sadd );
+    			str = cr.get(sadd);
+
+				if (str.indexOf("lost")>0)
+					System.out.println("-----------------------------");
+				System.out.println( str );
+				newplay = g.fromJson(str, StplayMT.class);
+				if (newplay.account.money>1000000)
+					System.out.println( "<<<<<<<<<<<<<<<<<    MT  MONEY = "+newplay.account.money+"   >>>>>>>>>>>>>>>>>>>" );
+
+			} while (newplay.account.money<1000000);
+
+
+		} catch (Exception e) { e.printStackTrace(); }
+
     }
 }
