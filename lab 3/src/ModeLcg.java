9c2cf254b61c4f3f2eaeb8b18786a05b398c42b6
Olha Chaika <62190177+CyberSeagull@users.noreply.github.com>
2022-01-16 20:34:06 +0200

    added a and c calculation

--- lab 3/src/ModeLcg.java
+++ lab 3/src/ModeLcg.java
@@ -29,15 +29,53 @@ public class ModeLcg {
 	{
     	return (int) ((_last*ac.a+ac.c) % m);
 	}
+    
+    static long ModInverse(long a, long m)
+    {
+    	long m0 = m;
+    	long y = 0, x = 1;
+        if (m == 1)  return 0;
+        while (a > 1) {
+        	long q = a / m;
+        	long t = m;
+            m = a % m; a = t;
+            t = y;
+            y = x - q * y;
+            x = t;
+        }
+        if (x < 0)
+            x += m0;
+        return x;
+    }    
 
     private Lcgac calculation_a_c() throws Throwable { 
     	newacc=cr.getplayerid(pathuri, g);
-    	String sadd=pathuri+String.format("/playLcg?id=%d&bet=%d&number=%d", newacc.id, 1, clientR.rnd(555));
-		String str = cr.get(sadd);
-  		Stplay newplay = g.fromJson(str, Stplay.class);
-       	newplay.reallong=Integer.toUnsignedLong(newplay.realNumber);
-      	m_play.add(newplay);
-        Lcgac ac = new Lcgac(111,222);
+    	long a=0, c=0;
+    	for (int i=0; i<3; i++) {
+    		String sadd=pathuri+String.format("/playLcg?id=%d&bet=%d&number=%d", newacc.id, 1, clientR.rnd(555));
+    	  	Stplay newplay = g.fromJson(cr.get(sadd), Stplay.class);
+    	  	newplay.reallong=Integer.toUnsignedLong(newplay.realNumber);
+    	  	m_play.add(newplay);
+    	  	if (i>=2) {
+    	  		a=m_play.get(i).reallong-m_play.get(i-1).reallong;
+        	if (a<0)
+//        		a=Integer.toUnsignedLong(  (int) a);
+    		    a=  (int) (a)+m;
+        	long modinv=m_play.get(i-1).reallong-m_play.get(i-2).reallong;
+        	if (modinv<0)
+        		modinv =(int) (modinv)+m;
+        	
+        	modinv = ModInverse(modinv,m);
+        	
+        	a=Integer.toUnsignedLong(  (int) ((a*modinv)% m) );
+        	c=Integer.toUnsignedLong(  (int) (m_play.get(i-1).reallong-m_play.get(i-2).reallong*a) );
+
+        	System.out.println("a= "+a+"   c="+c);
+    	}
+    	
+    	}
+    	
+        Lcgac ac = new Lcgac(a, c);
 		return ac;
     }
 	
@@ -46,7 +84,7 @@ public class ModeLcg {
 		try { 
 			Lcgac ac= calculation_a_c();
 	    	newplay = m_play.get(m_play.size()-1);
-	    	for (int i=0; i<2; i++) { 
+	    	for (int i=0; i<17; i++) { 
 				int realn = getnextnumber(newplay.reallong, ac);
 	    	    String sadd=pathuri+String.format("/playLcg?id=%d&bet=%d&number=%d", newplay.account.id, bet, realn);
     //	        System.out.println( sadd );
